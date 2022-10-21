148f5ea93953f5e9b7f965b97e9948be6c3696b2
RamiSketcher <ramisketcher@gmail.com>
2022-10-21 18:42:28 +0300

    update pixel

---
+++ pixel/scripts/atari_vector.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=atrai_vector
+
+#SBATCH --partition=p100
+
+#SBATCH --gres=gpu:0
+
+#SBATCH --qos=normal
+
+#SBATCH --cpus-per-task=4
+
+#SBATCH --mem-per-cpu=1G
+
+# prepare your environment here
+module load cuda-11.3
+
+# put your command here
+conda activate pixel
+
+python -m pixel.run --env "ALE/Pong-v5" --n-envs 2 --device 'cuda' --seed 1 --wb
