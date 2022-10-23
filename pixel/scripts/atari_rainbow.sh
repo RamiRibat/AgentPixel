cc14aed94f95f36eb70912a6f3177d68d1544313
RamiSketcher <ramisketcher@gmail.com>
2022-10-23 15:43:14 +0300

    update pixel

--- pixel/scripts/atari_rainbow.sh
+++ pixel/scripts/atari_rainbow.sh
@@ -1,4 +1,6 @@
 
+conda activate pixel
+
 python -m pixel.run --env "ALE/Pong-v5" --n-envs 64 --device 'cuda' --wb --seed 1
 # python -m pixel.run --env "ALE/Pong-v5" --n-envs 64 --device 'cuda' --wb --seed 2
 # python -m pixel.run --env "ALE/Pong-v5" --n-envs 64 --device 'cuda' --wb --seed 3
