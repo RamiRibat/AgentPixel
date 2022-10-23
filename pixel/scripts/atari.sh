ec7f4440e9053d6c6604babcf7270bb5cce53cd4
RamiSketcher <ramisketcher@gmail.com>
2022-10-23 15:42:49 +0300

    update pixel

--- pixel/scripts/atari.sh
+++ pixel/scripts/atari.sh
@@ -11,7 +11,3 @@ python -m pixel.run -cfg atari_rainbow -seed 1 -wb
 # python -m pixel.run -cfg atari_dqnvec -seed 1 -wb
 # python -m pixel.run -cfg atari_dqnvec -seed 2 -wb
 # python -m pixel.run -cfg atari_dqnvec -seed 3 -wb
-
-
-
-python -m pixel.run --env "ALE/Pong-v5" --n-envs 0 --device 'cuda'
---
+++ pixel/scripts/atari_rainbow.sh
@@ -0,0 +1,4 @@
+
+python -m pixel.run --env "ALE/Pong-v5" --n-envs 64 --device 'cuda' --wb --seed 1
+# python -m pixel.run --env "ALE/Pong-v5" --n-envs 64 --device 'cuda' --wb --seed 2
+# python -m pixel.run --env "ALE/Pong-v5" --n-envs 64 --device 'cuda' --wb --seed 3
