9d126a2347af753cb4fee390907c8a85abec9b4a
RamiSketcher <ramisketcher@gmail.com>
2022-11-05 01:31:30 +0300

    update derainbow-20

--- pixel/envs/wrappers/atari_preprocessing.py
+++ pixel/envs/wrappers/atari_preprocessing.py
@@ -136,12 +136,12 @@ class AtariPreprocessing(gym.Wrapper):
             total_reward += reward
             self.game_over = terminated
 
-            if self.terminal_on_life_loss and not terminated:
-                new_lives = self.ale.lives()
-                self.life_terminated = new_lives < self.lives and new_lives > 0
-                terminated = terminated or self.life_terminated #new_lives < self.lives
-                self.game_over = terminated
-                self.lives = new_lives
+            # if self.terminal_on_life_loss and not terminated:
+            #     new_lives = self.ale.lives()
+            #     self.life_terminated = new_lives < self.lives and new_lives > 0
+            #     terminated = terminated or self.life_terminated #new_lives < self.lives
+            #     self.game_over = terminated
+            #     self.lives = new_lives
 
             # if self.terminal_on_life_loss:
             #     new_lives = self.ale.lives()
