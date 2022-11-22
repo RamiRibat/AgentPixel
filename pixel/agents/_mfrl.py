fd67438c1d55f3ba8a61a8acc8fc4c9b7bac2425
RamiSketcher <ramisketcher@gmail.com>
2022-11-22 21:15:32 +0300

    pixel-v27

--- pixel/agents/_mfrl.py
+++ pixel/agents/_mfrl.py
@@ -92,7 +92,7 @@ class MFRL:
     def interact_vec(self, observation, mask, Z, L, T, Traj, epsilon=0.001):
         n_envs = self.configs['environment']['n-envs']
         xT = self.configs['learning']['expl-steps']
-        EP = [ epsilon - 0.005*n for n in range(n_envs) ]
+        EP = [ epsilon - 0.01*n for n in range(n_envs) ]
 
         if T > xT:
             # action = self.agent.get_action(observation)
--- pixel/agents/rainbow.py
+++ pixel/agents/rainbow.py
@@ -409,7 +409,7 @@ def main(configurations, seed, device, wb):
     # group_name = f"{algorithm}-200M-{environment}" # H < -2.7
 
     if n_envs > 0:
-        group_name = f"{algorithm}-{environment}-X{n_envs}-v26"
+        group_name = f"{algorithm}-{environment}-X{n_envs}-v27"
     else:
         group_name = f"{algorithm}-{environment}"
 
