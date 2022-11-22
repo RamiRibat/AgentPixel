86edd518fa3ba9d9e0538341851946ea3d4dcca9
RamiSketcher <ramisketcher@gmail.com>
2022-11-22 16:25:02 +0300

    pixel-v23

--- pixel/configs/atari_derainbowx4.py
+++ pixel/configs/atari_derainbowx4.py
@@ -40,7 +40,7 @@ configurations = {
 
     'learning': {
         'total-steps': int(200e3), # 100k in van Hasselt et al. (2019)
-        'init-steps': int(4000), # 1600 in van Hasselt et al. (2019)
+        'init-steps': int(10000), # 1600 in van Hasselt et al. (2019)
         'expl-steps': int(8000), # 0 in van Hasselt et al. (2019)
         'learn-freq': 1, # iteration
         'grad-steps': 2,
