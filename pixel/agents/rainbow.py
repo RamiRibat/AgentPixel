4d3a4aab11089b830ee1e29a5dc439ac1a4492fb
RamiSketcher <ramisketcher@gmail.com>
2022-11-23 14:39:21 +0300

    pixel-v37

--- pixel/agents/rainbow.py
+++ pixel/agents/rainbow.py
@@ -407,7 +407,7 @@ def main(configurations, seed, device, wb):
     # group_name = f"{algorithm}-200M-{environment}" # H < -2.7
 
     if n_envs > 0:
-        group_name = f"{algorithm}-{environment}-X{n_envs}-v36"
+        group_name = f"{algorithm}-{environment}-X{n_envs}-v37"
     else:
         group_name = f"{algorithm}-{environment}"
 
